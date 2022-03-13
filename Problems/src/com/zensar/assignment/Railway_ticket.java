b6eec907138ce3fbecc1610c8a00c790d1ef7572
pavankumar0077 <40380941+pavankumar0077@users.noreply.github.com>
2022-03-13 12:29:10 +0530

    Update Railway_ticket.java

--- Problems/src/com/zensar/assignment/Railway_ticket.java
+++ Problems/src/com/zensar/assignment/Railway_ticket.java
@@ -1,28 +1,38 @@
 package com.zensar.assignment;
 
-public class Railway_ticket extends Tickets{
-	
-	
-	private String seatType;
+//Inheritance
+public class Railway_ticket extends Tickets {
 
+	private String seatType;
 
 	public Railway_ticket(String from, String to, int seatNo, float price, float agent_commission, String seatType) {
 		super(from, to, seatNo, price, agent_commission);
 		this.seatType = seatType;
 	}
-	
+
+	public void total_ticket_price() {
+
+		float price = 0;
+		float agent_commission = 0.0f;
+
+		float price_prtge = this.agent_commission * this.price;
+		float total_prtge = this.price + price_prtge;
+
+		System.out.println("Total price of the ticket is : " + total_prtge);
+
+	}
+
+	// Polymorphism
 	public void ticket_details() {
 		System.out.println("Railway Ticket Details...");
 		System.out.println("Welcome.. <================>");
-		System.out.println("From which place : " +from);
-		System.out.println("To which place : " +to);
-		System.out.println("Seat Number of the passenger is : " +seatNo);
-		System.out.println("Price of the ticket : " +price);
-		System.out.println("Agents commission is : " +agent_commission);
-		System.out.println("Your seatType is : " +seatType);
-		
+		System.out.println("From which place : " + from);
+		System.out.println("To which place : " + to);
+		System.out.println("Seat Number of the passenger  : " + seatNo);
+		System.out.println("Price of the ticket : " + price);
+		System.out.println("Agent's commission  : " + agent_commission);
+		System.out.println("Your seatType  : " + seatType);
+
 	}
-	
-	
 
 }
