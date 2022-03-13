98566791d40cb22c4e96246dbe71872156018ea7
pavankumar0077 <40380941+pavankumar0077@users.noreply.github.com>
2022-03-13 12:29:33 +0530

    Update Flight_ticket.java

--- Problems/src/com/zensar/assignment/Flight_ticket.java
+++ Problems/src/com/zensar/assignment/Flight_ticket.java
@@ -1,7 +1,7 @@
 package com.zensar.assignment;
 
-public class Flight_ticket extends Tickets{
-	
+// Inheritance
+public class Flight_ticket extends Tickets {
 
 	protected float luggage_weight;
 
@@ -10,70 +10,39 @@ public class Flight_ticket extends Tickets{
 		super(from, to, seatNo, price, agent_commission);
 		this.luggage_weight = luggage_weight;
 	}
-	
-	
+
+	// polymorphism
 	public void ticket_details() {
 		System.out.println("Flight Ticket Details...");
 		System.out.println("Welcome.. <================>");
-		System.out.println("From which place : " +from);
-		System.out.println("To which place : " +to);
-		System.out.println("Seat Number of the passenger is : " +seatNo);
-		System.out.println("Price of the ticket : " +price);
-		System.out.println("Agents commission is : " +agent_commission);
-		System.out.println("Luggage weight is : " +luggage_weight);
+		System.out.println("From which place : " + from);
+		System.out.println("To which place : " + to);
+		System.out.println("Seat Number of the passenger  : " + seatNo);
+		System.out.println("Price of the ticket : " + price);
+		System.out.println("Agent's commission  : " + agent_commission);
+		System.out.println("Luggage weight  : " + luggage_weight);
 	}
-	
+
 	public void fligt_insurance() {
 		final float insurance_percentage = (0.01f * this.price);
-		final float total_ins_per = this.price - insurance_percentage;
-		System.out.println("Price of the ticket with insurance is : "+total_ins_per);
+		final float total_ins_per = this.price + insurance_percentage;
+		System.out.println("Price of the ticket with insurance 1% is : " + total_ins_per);
 	}
-	
+
 	public void flight_total_ticket_price() {
-//		this.luggage_weight = 0;
 		if (this.luggage_weight > 15) {
 			final float total_percentage = (0.03f * this.price);
 			final float total_price = this.price + total_percentage;
-			System.out.println("Total ticket price with luggage > 15kg of luggage is : "+total_price);
-		}else {
-			System.out.println("Total ticket price with luggage < or = 15kg of luggage is : "+this.price);
+			System.out.println("Total ticket price with luggage > 15kg of luggage is : " + total_price);
+		} else {
+			System.out.println("Total ticket price with luggage < or = 15kg of luggage is : " + this.price);
 		}
 	}
-	
-	public void final_ticket_price () {
+
+	public void final_ticket_price() {
 		fligt_insurance();
 		flight_total_ticket_price();
-		
-//		final float insurance_percentage = (0.01f * this.price);
-//		final float total_ins_per = this.price - insurance_percentage;
-//		System.out.println("Price of the ticket with insurance is : "+total_ins_per);
-//		
-//		
-//		if (this.luggage_weight > 15) {
-//			final float total_percentage = (0.03f * this.price);
-//			final float total_price = this.price + total_percentage;
-//			System.out.println("Total ticket price with > 15kg of luggage is : "+total_price);
-//		}else {
-//			System.out.println("Total ticket price with < or = 15kg of luggage is : "+this.price);
-//		}
-		
-//		float total_price = 0;
-//		float grand_total_price = (total_ins_per + total_price);
-//		
-//		
-//		System.out.println("Total price with insurance and luggage > 15kgs  : " +grand_total_price);
-		
-		
-		
+
 	}
-		
+
 }
-	
-
-	
-
-		
-	
-	
-
-
